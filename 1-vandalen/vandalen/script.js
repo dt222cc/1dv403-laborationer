eb95c29121feb338be14c4bc4da1fa771f3e0539
Da Sing Trinh <dt222cc@student.lnu.se>
2014-11-08 17:26:04 +0000

    Alla testar avklarade, ska piffa upp koden lite och försöka implementera felhantering.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -6,18 +6,29 @@
         var result = {};
         var names;
     	var ages;
-    	var minAge;
         var maxAge;
+    	var minAge;
     	var averageAge;
+    	var sum;
         
         // Osäker på hur man ska lägga upp koden, testar med denna.
         // Samlar alla namn i en plats.
         names = persArr.map(function(eachpers){return eachpers.name;
             // Sortera namnen.
         }) .sort(function(a,b){return a.localeCompare(b);
-            // Sätter ihop dem, istället för ny rad för varje namn.
-        }) .join(", ");
-            
+        }) .join(", "); // Sätter ihop egenskaperna till en objekt.
+        
+        // Samlar alla ålder i en plats.
+        ages = persArr.map(function(eachage){return eachage.age;});
+        
+        maxAge = Math.max.apply(Math, ages); // Väljer ut det högsta åldern.
+       
+        minAge = Math.min.apply(Math, ages); // Väljer ut det minsta åldern.
+        
+        // Kalkylerar medelåldern.
+        sum = ages.reduce(function(a, b){return a + b});
+        averageAge = Math.round(sum / ages.length); 
+        
         // Returnerar resultat.
         result = {minAge: minAge, maxAge: maxAge, averageAge: averageAge, names: names};
         return result;
@@ -32,4 +43,8 @@
     // Referenser
     // Sortering - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
     // Kombinera elementer till en sträng - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+    // maxAge - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+    // minAge - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/min
+    // averageAge   - http://stackoverflow.com/questions/10359907/array-sum-and-average
+    //        och   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
     
