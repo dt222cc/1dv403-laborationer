5475c76eaeb47b9c0901a4558c866f4a18564374
Da Sing Trinh <dt222cc@student.lnu.se>
2014-11-08 16:04:18 +0000

    L01.4 Vandelen - Start

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,20 @@
 "use strict";
 
-var makePerson = function(persArr){
-
-
-	// Din kod här...
-
-}
-
+    var makePerson = function(persArr){
+        
+        // Variabler som kan behövas.
+        var result = {};
+        var names;
+    	var ages;
+    	var minAge;
+        var maxAge;
+    	var averageAge;
+    	
+        // Returnerar resultat.
+        return result;
+    };
+    
+    // För att testa resultatet.
+    var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+	var result = makePerson(data);
+    console.log(result);
