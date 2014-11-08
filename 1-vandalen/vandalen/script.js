b2669bc2d12c2027faa1fe2ff0ec5f8249b2ed7d
Da Sing Trinh <dt222cc@student.lnu.se>
2014-11-08 18:35:51 +0000

    Har försökt använda typeof men får inte till det. Försöker igen en annan gång.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,7 +2,7 @@
 
     var makePerson = function(persArr){
         
-        // Osäker på hur man ska lägga upp koden, testar med denna.
+        // Osäker på hur man ska lägga upp koden, testar med denna struktur.
         // Samlar alla namn i en plats.
         var names = persArr.map(function(eachpers){return eachpers.name;
             // Sortera namnen.
@@ -28,14 +28,15 @@
     // För att testa resultatet.
     var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
 	var result = makePerson(data);
+	
     console.log(result);
     
-    
     // Referenser
-    // Sortering - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    // Sortering    - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
     // Kombinera elementer till en sträng - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
-    // maxAge - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
-    // minAge - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/min
+    // maxAge       - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+    // minAge       - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/min
     // averageAge   - http://stackoverflow.com/questions/10359907/array-sum-and-average
-    //        och   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
-    
+    //              - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+    // Felhantering - http://stackoverflow.com/questions/1303646/check-whether-variable-is-number-or-string-in-javascript
+    //              - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
