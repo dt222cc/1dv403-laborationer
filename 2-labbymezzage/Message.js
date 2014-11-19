0cef4dd5449feabb4664a8368fb8da1c459012f9
Da Sing Trinh <dt222cc@student.lnu.se>
2014-11-19 06:01:45 +0000

    started work with code - step 1-2

--- 2-labbymezzage/Message.js
+++ 2-labbymezzage/Message.js
@@ -1,1 +1,38 @@
 "use strict";
+
+function Message(message, date){
+    
+    // Konstruktor för meddelanden
+    this.getText = function(){
+        return message;
+    };
+    
+    this.setText = function(_text){
+        message = _text;
+    };
+    
+    // Konstruktor för datum
+    this.getDate = function(){
+        return date;
+    };
+    
+    this.setDate = function(_date){
+        date = _date;
+    };
+}
+    
+    // Strängrepresentation av objektet
+    Message.prototype.toString = function(){
+        return this.getText() +" ("+this.getDate()+")";
+    };
+    
+    // Hämtar texten med \n och byter ut den mot <br />
+    Message.prototype.getHTMLText = function(){
+        return this.getText().replace(/\n/g, "<br>");
+        // http://stackoverflow.com/questions/5076466/javascript-replace-n-with-br
+    };
+    
+    // 
+    Message.prototype.getDateText = function(){
+        return " ("+this.getDate()+")"; // Ska modifieras vidare? bort med GMT
+    };
--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -1,6 +1,13 @@
 "use strict";
 
 window.onload = function(){
-  
 
+    // Skapa meddelanden, test (funkar)
+    var mess = new Message("Testmeddelande", new Date());
+    
+    alert(mess);
+    alert(mess.getText());
+    mess.setText("En annan text");
+    alert(mess);
+    alert(mess.getText());
 };
