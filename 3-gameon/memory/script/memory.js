c417a823b8f324935e424bd2ffcecec0cb7fe752
Da Sing Trinh <dt222cc@student.lnu.se>
2014-12-01 22:40:46 +0000

    can now flip tiles. continuing with function checkMatch.

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -5,21 +5,25 @@ var cols = 4;
 
 var Memory = {
     
+    gameArray: [],
+    turnedPictures: [],
+    
     init: function(){
         
-        var newGame = RandomGenerator.getPictureArray(rows, cols); // Retrieves and stores an array
-        console.log(newGame);
+        Memory.gameArray = RandomGenerator.getPictureArray(rows, cols); // Retrieves and stores an array
+        console.log(Memory.gameArray);
         
         Memory.buildBoard();
     },
     
     // Builds a board for the game
     buildBoard: function(){
-
-        var game = document.getElementById("board");
+        
+        var turnCount = 0;
+        var board = document.getElementById("board");
         var table = document.createElement("table");
         
-        game.appendChild(table);
+        board.appendChild(table);
         
         // Creates rows for table
         for (var row = 0; row < rows; row++){
@@ -41,29 +45,67 @@ var Memory = {
                 
                 a.appendChild(img), td.appendChild(a), tr.appendChild(td);
                 
-                this.clickEvent(a);
+                Memory.clickEvent(a, turnCount);
+                
+                turnCount++;
             }
         }
     },
     
-    clickEvent: function(a){
+    clickEvent: function(aTagg, count){
         
         // Mouse click
-        a.addEventListener("click", function(){
-            console.log("Hej");
+        aTagg.addEventListener("click", function(e){
+            console.log("click");
+            Memory.turnPicture(aTagg, count);
         });
         
         // Keypress, enter
-        a.addEventListener("keydown", function(key){
+        aTagg.addEventListener("keydown", function(key){
             if (key.keyCode === 13){
-                // console.log("extra"); // bug: adds mouse click aswell
-                // Works empty, why?
+                console.log("press");
+                key.preventDefault();
+                Memory.turnPicture(aTagg, count);
             }
         });
-        
     },
     
+    turnPicture: function(tile, turnCount){
+        
+        console.log("flip");
+        
+        var img = tile.getElementsByTagName("img")[0];
+        
+        // Can not turn what is already turned
+        if (img.getAttribute("src") !== "pics/0.png"){
+            return false;
+        }
+        
+        Memory.turnedPictures.push(tile); // Store flipped tiles in an array (for function checkMatch)
+        
+        // Flip tile and show tile img
+        if (Memory.turnedPictures.length <= 2){
+            
+            console.log(Memory.gameArray[turnCount]);
+            
+            img.getAttribute("src");
+            img.setAttribute("src", "pics/" + Memory.gameArray[turnCount] + ".png");
+        }
+        
+        // Two tiles are flipped > time to check for match
+        if (Memory.turnedPictures.length === 2){
+            
+            // TODO - Timeout
+            Memory.checkMatch(Memory.turnedPictures);
+        }
+    },
     
+    checkMatch: function(){
+        
+        console.log("checking");
+        
+        
+    },
 };
 
-window.onload = Memory.init;
+window.onload = Memory.init();
