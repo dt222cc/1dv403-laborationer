8ac9e1424908f7d6f55cda608536afafb9254f55
Da Sing Trinh <dt222cc@student.lnu.se>
2014-12-04 14:20:58 +0000

    update - clean code, comments, renames, etc

--- 3-gameon/memory/scriptStar/memory.js
+++ 3-gameon/memory/scriptStar/memory.js
@@ -1,68 +1,57 @@
 "use strict";
 
-function Memory(row, col, gameID){
+function Memory(rows, cols, gameID){
     
-    var rows = row;
-    var cols = col;
-    var gameArray = [];
     var turnedPictures = [];
     var guesses = 0;
     var matches = 0;
     var board = document.getElementById(gameID);
     var reset = document.getElementById("reset");
     
-    gameArray = RandomGenerator.getPictureArray(rows, cols); // Retrieves and stores an array
+    var gameArray = RandomGenerator.getPictureArray(rows, cols); // Retrieves and stores an array
     console.log(gameArray);
     
-    playAgain(reset);
-    
-    buildBoard(gameID);
+    resetButton();
+    buildBoard();
 
     // Builds a board for the game
     function  buildBoard(){
         
-        var turnCount = 0;
-        
+        var imgCount = 0;
         var table = document.createElement("table");
-        
-        board.appendChild(table);
         
         // Creates rows for table
         for (var row = 0; row < rows; row++){
             
             var tr = document.createElement("tr");
             
-            table.appendChild(tr);
-            
             // Creates a columns for each row
             for (var col = 0; col < cols; col++){
               
                 var td = document.createElement("td");
-                
                 var a = document.createElement("a");
-                a.setAttribute("href", "#");
-                
                 var img = document.createElement("img");
+                
+                a.setAttribute("href", "#");
                 img.src = "../pics/0.png";
                 
                 a.appendChild(img), td.appendChild(a), tr.appendChild(td);
                 
-                clickEvent(a, turnCount);
-                
-                turnCount++;
+                clickEvent(a, imgCount);
+                imgCount++;
             }
+            table.appendChild(tr);
         }
+        board.appendChild(table);
     }
     
-    // Click event for mouse click and keypress (enter)
+    // Event listener for <a> (mouse click and keypress: enter)
     function clickEvent(aTagg, count){
         
-        // Mouse click
-        aTagg.addEventListener("click", function(e){
+        aTagg.addEventListener("click", function(){
             turnPicture(aTagg, count);
         });
         
-        // Keypress, enter
         aTagg.addEventListener("keydown", function(key){
             if (key.keyCode === 13){
                 key.preventDefault();
@@ -72,7 +61,7 @@ function Memory(row, col, gameID){
     }
     
     // Turns selected tile/tiles
-    function turnPicture(tile, turnCount){
+    function turnPicture(tile, imgCount){
         
         var img = tile.getElementsByTagName("img")[0];
         
@@ -81,58 +70,62 @@ function Memory(row, col, gameID){
             return false;
         }
         
-        turnedPictures.push(tile); // Store flipped tiles in an array (for function checkMatch)
+        turnedPictures.push(tile); // Store flipped tiles in an array to check if they match or not
         
         // Flip tile and show/get tile img
         if (turnedPictures.length <= 2){
-            console.log(gameArray[turnCount]);
+            console.log(gameArray[imgCount]);
             
             img.getAttribute("src");
-            img.setAttribute("src", "../pics/" + gameArray[turnCount] + ".png");
+            img.setAttribute("src", "../pics/" + gameArray[imgCount] + ".png");
         }
         
-        // Two tiles are flipped > time to check for match
+        // When two tiles have been flipped: checkMatch
         if (turnedPictures.length === 2){
+            // Delay here: 900ms then checkMatch
             setTimeout(function(){
                 checkMatch(turnedPictures);
             }, 900);
         }
     }
     
-    // Checks if selected two tiles match
+    // Checks if selected two tiles match.
+    // After each check the array gets emptied for the next set of check.
     function checkMatch(tiles){
 
         guesses++;
         
-        // If tiles match, they stay turned
+        // If tiles match: they stay turned then reset the array.
         if (tiles[0].getElementsByTagName("img")[0].getAttribute("src") === tiles[1].getElementsByTagName("img")[0].getAttribute("src")){
-            turnedPictures = []; // Array gets emptied for next set of guess
+            turnedPictures = [];
             matches++;
             
+            // Game done (not optimal solution)
             if (matches === (rows * cols / 2)){
                 var text = document.createElement("p");
-                text.innerHTML = ("Grattis! Du klarade spelet på " +guesses+ " försök.");
+                text.innerHTML = ("Grattis!<br />Försök: " +guesses);
                 board.appendChild(text);
             }
         }
-        // If not a match, flip them back to the default "backside" image
+        // If not a match: flip them back to the "backside" image and reset the array.
         else {
             tiles[0].getElementsByTagName("img")[0].setAttribute("src", "../pics/0.png");
             tiles[1].getElementsByTagName("img")[0].setAttribute("src", "../pics/0.png");
             
-            turnedPictures = []; // Array gets emptied for next set of guess
+            turnedPictures = [];
         }
+        console.log("Tries  : " +guesses);
+        console.log("Matches: " +matches);
     }
     
-    function playAgain(yes){
+    // Event listener for reset button (mouseclick and keypress: enter)
+    function resetButton(){
         
-        // Mouse click
-        yes.addEventListener("click", function(e){
+        reset.addEventListener("click", function(){
             location.reload();
         });
         
-        // Keypress, enter
-        yes.addEventListener("keydown", function(key){
+        reset.addEventListener("keydown", function(key){
             if (key.keyCode === 13){
                 key.preventDefault();
                 location.reload();
